08f1ec0adc5bacf3db81ca1e4b51e0dc486ddfc5
@Royal-Reddy-Yaparla <iamroyalreddy@gmail.com>
2023-08-11 08:53:36 +0530

    variable

---
+++ Basic/Variables/go.mod
@@ -0,0 +1,3 @@
+module golang/basic/variable
+
+go 1.19
---
+++ Basic/Variables/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	// define with type
+	var number int = 5
+	fmt.Println("Integer", number)
+	// define with datatype declaration or short method
+	name := "Golang"
+	fmt.Println("Name:", name)
+}
