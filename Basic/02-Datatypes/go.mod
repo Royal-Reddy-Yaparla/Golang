80401303852d5ac1db7d88dfcd7b0cc27efd93e9
@Royal-Reddy-Yaparla <iamroyalreddy@gmail.com>
2023-08-11 09:09:22 +0530

    added datatypes

---
+++ Basic/02-Datatypes/go.mod
@@ -0,0 +1,3 @@
+module golang/basics/datatypes
+
+go 1.19
---
+++ Basic/02-Datatypes/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Integers
+	var integer int = 5
+	fmt.Println("Integer: ", integer)
+
+	// Float
+	var floatNumber float64 = 5.5
+	fmt.Println("Float number: ", floatNumber)
+
+	// String
+	var name string = "Golang"
+	fmt.Println("Name: ", name)
+
+	// Boolean
+	var isItBool bool = true
+	fmt.Println("IsItBoolean: ", isItBool)
+}
