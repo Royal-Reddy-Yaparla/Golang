fd48e6e11cff3c857c5367b36d05435ae09ac048
@Royal-Reddy-Yaparla <iamroyalreddy@gmail.com>
2023-08-11 08:55:40 +0530

    updated

--- Basic/Variables/main.go
+++ Basic/Variables/main.go
@@ -3,10 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	// define with type
+	// define with datatype
 	var number int = 5
 	fmt.Println("Integer", number)
-	// define with datatype declaration or short method
+	// define without datatype declaration or short method
 	name := "Golang"
 	fmt.Println("Name:", name)
 }
